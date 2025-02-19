8aef11b2f45f256f903c418e697e01844f8bd8df
Shahid-cs22 <mohamrdshahid.cs22@stellamarysceo.edu.in>
2025-02-19 21:04:19 +0530

    day 5

---
+++ function.py
@@ -0,0 +1,13 @@
+def add():
+    print("Addition")
+    a=int(input("Enter first number:"))
+    b=int(input("Enter second number:"))
+    print(a+b)
+
+def sub():
+    print("Subtraction")
+    a=int(input("Enter first number:"))
+    b=int(input("Enter second number:"))
+    print(a-b)
+add()
+sub()
